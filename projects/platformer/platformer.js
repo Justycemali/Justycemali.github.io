cae4f990ec0f15c31d147ed3f56f54b564f8ceb5
Justycemali sherman <Justyce.Sherman@Sciacademy.org>
2023-09-12 15:21:52 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -52,7 +52,7 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-      createCollectable("database", 600,350,55);
+      createCollectable("database", 550,350,85,);
 
 
 
@@ -61,7 +61,7 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+       createCannon("right",750,1500)
 
 
 
